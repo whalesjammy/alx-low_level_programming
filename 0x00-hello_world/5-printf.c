b4352f14a1046e50b0f309a18a65795b14cca92b
whales <whalesjammy@gmail.com>
2023-08-20 05:48:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - print out a string to stdout.
+ * Return: 0 if successs.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
